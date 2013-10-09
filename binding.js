53af9f81c35f832a0690e5f34ce0733137298c68
Scott J. Miles <sjmiles@google.com>
2013-10-09 13:54:08 -0700

    various updates

--- binding.js
+++ binding.js
@@ -3,7 +3,6 @@
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
-
 (function() {
  function getBinding(element, name) {
     if (element && element.bindings) {
@@ -13,8 +12,6 @@
       }
     }
   }
- 
-  
   window.Bindings = {
     getBinding: getBinding
   }
